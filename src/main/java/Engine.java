f909273a2e568bb2bc0345cf604d7f9a75abefcd
Danny Nguyen <58054670+friedice5467@users.noreply.github.com>
2022-03-30 09:51:32 -0700

    Add files via upload

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,2 +1,15 @@
+import java.util.Random;
+
 public class Engine {
+
+    private static final int scorePoints2Max = 20;
+    private static final int scorePoints2Min = 1;
+
+    public static int scoredPoints(){
+        Random random = new Random();
+        if(random.nextInt(scorePoints2Max-scorePoints2Min) + Player.getShooting() >= 20) return 2;
+
+        return 0;
+    }
+
 }
