e484a746c94cc9b1afc57ad6d5496349eec2b6c4
Danny Nguyen <58054670+friedice5467@users.noreply.github.com>
2022-04-12 08:19:54 -0700

    Add files via upload

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,17 +1,38 @@
-public class Team {
-    private String teamName;
-    private int playerNumber;
+class Team {
+    private Player[] roster;
+    private int offenseScore;
+    private int defenseScore;
 
-    public String getTeamName() {
-        return teamName;
+    public Team(Player[] roster, int offenseScore, int defenseScore) {
+        this.roster = roster;
+        this.offenseScore = offenseScore;
+        this.defenseScore = defenseScore;
     }
 
-    public int getPlayerNumber() {
-        return playerNumber;
+    public int playPossession(Team team1, Team team2){
+        int teamOffense = (int) (Math.random()*team1.offenseScore);
+        int teamDefense = (int) (Math.random()*team2.defenseScore);
+
+        if(teamOffense > teamDefense) return 2;
+        return 0;
     }
 
-    public Team(String teamName, int playerNumber) {
-        this.teamName = teamName;
-        this.playerNumber = playerNumber;
+    public Team makeRandomTeam(){
+        int offensePlayer = 0;
+        int defensePlayer = 0;
+        roster = new Player[5];
+
+        for (int i = 0; i < 5; i++) {
+            Player player = new Player(offensePlayer, defensePlayer);
+            player.makeRandomPlayer();
+            offensePlayer += player.getOffense();
+            defensePlayer += player.getDefense();
+            roster[i] = player;
+        }
+
+        offenseScore = offensePlayer / 5;
+        defenseScore = defensePlayer / 5;
+
+        return new Team(roster, offenseScore, defenseScore);
     }
 }
